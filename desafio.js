21be6504cd31b109fc7fd5b13d32f3b2e1a587d3
BersaneV <victorbersanej@gmail.com>
2025-10-21 19:36:41 -0300

    sistema de xp

---
+++ desafio.js
@@ -0,0 +1,29 @@
+let xpPlayer = 0;
+let nomeDoPlayer = "BaiacudeCueca";
+let xpMaximo = 10001;
+let rankPlayer = " ";
+if (xpPlayer >= 0 && xpPlayer <= 1000){
+rankPlayer = "Ferro!"
+}
+else if (xpPlayer > 1000 && xpPlayer <=2000){
+rankPlayer = "Bronze!"
+}
+else if (xpPlayer > 2000 && xpPlayer <=5000){
+rankPlayer = "Prata!"
+}
+else if (xpPlayer > 5000 && xpPlayer <= 7000){
+rankPlayer = "Ouro!"
+}
+else if (xpPlayer > 7000 && xpPlayer <= 8000){
+rankPlayer = "Platina!"
+}
+else if (xpPlayer > 8000 && xpPlayer <= 9000){
+rankPlayer = "Ascendente!"
+}
+else if (xpPlayer > 9000 && xpPlayer <= 10000){
+rankPlayer = "Imortal!"
+}
+else if (xpPlayer >= 10000){
+rankPlayer = "Radiante!"
+}
+console.log (" O Herói " + nomeDoPlayer + " Possui " + xpPlayer + " de XP, e seu rank é: " + rankPlayer)
